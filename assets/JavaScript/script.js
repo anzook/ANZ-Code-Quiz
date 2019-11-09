dd5fb5ec38904efaf90dbf2db91ab40ba52917dc
anz213 <56008225+anzook@users.noreply.github.com>
2019-11-09 14:32:21 -0500

    added question format

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -9,12 +9,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     var initCard = document.getElementById("initalCard");
     var questCard = document.getElementById("questionCard");
+    var timerDisp = document.getElementById("timer");
+
+    var timer = 75;
     
 
     startBtn.addEventListener("click", function() {
         initCard.style.display = "none";
         questCard.style.display = "block"
+        beginGame();
       });
+
+function beginGame() {
+
+    interval = setInterval(function() {
+        timer--;
+        timerDisp.textContent = timer;
+      }, 1000);
+
+
+}
 
 
 });
