36ee4130ab7b124ea0c8df864e0ee92f0373fcb1
anz213 <56008225+anzook@users.noreply.github.com>
2019-11-10 00:04:55 -0500

    stuck on event bug and adding scoring

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,8 +1,3 @@
-// var artDiv = document.getElementById("articles");
-// var mainDiv = document.getElementById("main");
-// document.querySelector("#change2")
-// document.getElementsByClassName("example");
-
 
 document.addEventListener("DOMContentLoaded", function (event) {
     var startBtn = document.getElementById("startButton");
@@ -10,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var initCard = document.getElementById("initalCard");
     var questCard = document.getElementById("questionCard");
     var timerDisp = document.getElementById("timer");
+    var alert = document.getElementById("alert");
     // var buttons = document.querySelectorAll(".btn")
 
     var questText = document.getElementById("questionPrompt");
@@ -26,9 +22,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var cBtn = document.getElementById("CAnswerBtn");
     var dBtn = document.getElementById("DAnswerBtn");
 
-    var problemTime = 15;
-    var timer = questions.length * problemTime;  //sets initial timer length
-
+    var problemTime = 15;    //time per question
+    var iter = 0;
+    var timer = 75;
+    var endTime = 0;
+    var correct = 0;
+    var wrong = 0;
+    var user = "";
+    var score = 0;
 
     /**
      * Randomize array element order in-place.
@@ -73,17 +74,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     function endGame() {
         clearInterval(interval);
+        endTime = timer;
         timer = 0;
         timerDisp.textContent = timer;
+        console.log("wins " + correct + ", Losses " + wrong + ", " + endTime);
         initCard.style.display = "block";
         questCard.style.display = "none";
+    var highscore = {
+        score: (correct * problemTime + endTime),
+    user: prompt(`Your score is ${correct * problemTime + endTime}. Enter your initials:`)  
     }
+
+}
 
 
     function beginGame() {
-        var correct = 0;
-        var wrong = 0;
-        var iter = 0;
+        correct = 0;
+        wrong = 0;
+        iter = 0;
+        timer = questions.length * problemTime;  //sets initial timer length
+
 
         let newQuestions = questions;
         newQuestions = shuffle(newQuestions);   //randomly arranges questions
@@ -91,45 +101,50 @@ document.addEventListener("DOMContentLoaded", function (event) {
         setQuestion(iter);
 
         interval = setInterval(function () {   //initializes timer
-            
+
             timerDisp.textContent = timer;
             timer--;
 
-            if ( timer < 0) {
+            if (timer < 0) {
                 endGame();
 
-            } else if ( iter >= newQuestions.length) {
+            } else if (iter >= newQuestions.length) {
                 endGame();
             }
 
 
         }, 1000);
 
-            
-            answerBtns.addEventListener("click", function (event) {
-                event.preventDefault();
-                if (event.target.matches(".btn")) {
-                    let userAnswer = event.target.nextElementSibling.textContent;
-                    // console.log(userAnswer);
-                    if (userAnswer === newQuestions[iter].answer) {
-                        console.log("win");
-                        correct++;                       
-                    } else {
-                        console.log("lose");
-                        timer -= 5; //5 seconds lost for wrong answer
-                        wrong++;
-                    }
-                    if (iter < (newQuestions.length - 1)){
-                        iter++;
-                        setQuestion(iter);
-                    } else { iter++; }
-                }
 
-            });
-            
+        answerBtns.addEventListener("click", function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            if (event.target.matches(".btn")) {
+                let userAnswer = event.target.nextElementSibling.textContent;
+                // console.log(userAnswer);
+                if (userAnswer === newQuestions[iter].answer) {
+                    console.log("win");
+                    console.log(iter);
+                    correct++;
+                } else {
+                    console.log("lose");
+                    console.log(iter);
+                    timer -= 5; //5 seconds lost for wrong answer
+                    wrong++;
+                }
+                if (iter < (newQuestions.length - 1)) {
+                    iter++;
+                    setQuestion(iter);
+                } else if (iter === (newQuestions.length - 1)) {
+                    iter++;
+                }
+            }
+
+        });
+
 
     }
 
 
-    });
+});
 
